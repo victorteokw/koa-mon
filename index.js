2226e6843bb24051e0ef4011a7b61d2b15c9663d
Zhang Kaiyu <yeannylam@gmail.com>
2018-06-27 12:56:03 +0800

    support multiple mongoose connections

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const mongoose = require('mongoose');
 const glob = require('glob');
 const chalk = require('chalk');
+const path = require('path');
 
-const locks = [];
-
-const lock = () => {
-  return new Promise(function(res, rej) {
-    locks.push(res);
-  });
-};
-
-const unlock = () => {
-  while (locks.length) {
-    const res = locks.pop();
-    res();
-  }
+const isCapitalized = function(s) {
+  const charCode = s.substr(0, 1).charCodeAt(0);
+  return charCode >= 65 && charCode <= 90;
 };
 
 const timestamp = function() {
@@ -35,21 +26,12 @@ const log = function(s) {
   console.log(`${chalk.blue(timestamp())}${chalk.magenta.bold('[DB]')} ${s}`);
 };
 
-const connect = function(url, options) {
-  log(`Connecting...`);
-  mongoose.connect(url, options).catch((err) => {});
-};
-
-module.exports = function({url, options, models, debug, lazyConnect}) {
-
-  log(`Using ${url}.`);
+module.exports = function({
+  url, options, modelDir, debug, lazyConnect, connectionName
+}) {
 
   // configure mongoose
-  mongoose.set('debug', debug);
-
-  // Load models
-  const modelPaths = glob.sync(models + '/**/*.js');
-  modelPaths.map(require);
+  mongoose.set('debug', mongoose.get('debug') || debug);
 
   // Connection Handlers
   const defaultOptions = {
@@ -58,32 +40,75 @@ module.exports = function({url, options, models, debug, lazyConnect}) {
     reconnectInterval: 2000
   };
 
-  let connected = false, reconnectTries = 0;
+  let connection = undefined, 
+    connected = false,
+    reconnectTries = 0,
+    eventsAttached = false;
+  const models = {};
 
-  mongoose.connection.on('error', () => {
-    connected = false;
-    log(`Error Occurred.`);
-    if (options.autoReconnect !== false) {
-      if (reconnectTries < (options.reconnectTries || defaultOptions.reconnectTries)) {
-        setTimeout(() => {
-          connect(url, options);
-          reconnectTries += 1;
-        }, options.reconnectInterval || defaultOptions.reconnectInterval);
-      }
+  const connect = function(url, options) {
+    log(`Using ${url}.`);
+    log(`Connecting...`);
+    if (connectionName) {
+      connection = mongoose.createConnection(url, options);
+    } else {
+      mongoose.connect(url, options).catch(() => {});
+      connection = mongoose.connection;
     }
-  });
+    if (!eventsAttached) {
+      connection.on('error', () => {
+        connected = false;
+        log(`Error Occurred.`);
+        if (options.autoReconnect !== false) {
+          if (reconnectTries <
+            (options.reconnectTries || defaultOptions.reconnectTries)) {
+            setTimeout(() => {
+              connect(url, options);
+              reconnectTries += 1;
+            }, options.reconnectInterval || defaultOptions.reconnectInterval);
+          }
+        }
+      });
 
-  mongoose.connection.on('disconnected', function () {
-    connected = false;
-    log(`Disconnected.`);
-  });
+      connection.on('disconnected', function () {
+        connected = false;
+        log(`Disconnected.`);
+      });
 
-  mongoose.connection.on('connected', function () {
-    connected = true;
-    reconnectTries = 0;
-    log(`Connected.`);
-    unlock();
-  });
+      connection.on('connected', function () {
+        connected = true;
+        reconnectTries = 0;
+        log(`Connected.`);
+        unlock();
+      });
+
+      // Load models
+      const modelPaths = glob.sync(modelDir + '/**/*.js');
+      const modelObjects = modelPaths.map(require);
+      modelObjects.map((o, i) => {
+        const name = path.parse(modelPaths[i]).name;
+        if ((o.constructor === mongoose.Schema) && isCapitalized(name)) {
+          models[name] = connection.model(name, o);
+        }
+      });
+      eventsAttached = true;
+    }
+  };
+
+  const locks = [];
+
+  const lock = () => {
+    return new Promise(function(res, rej) {
+      locks.push(res);
+    });
+  };
+
+  const unlock = () => {
+    while (locks.length) {
+      const res = locks.pop();
+      res();
+    }
+  };
 
   if (!lazyConnect) {
     connect(url, options);
@@ -92,7 +117,15 @@ module.exports = function({url, options, models, debug, lazyConnect}) {
   // Middleware setup
   return async function(ctx, next) {
     ctx.mongoose = mongoose;
-    ctx.models = ctx.mongoose.models;
+    if (connectionName) {
+      ctx.connections = Object.assign(
+        {},
+        ctx.connections,
+        { [connectionName]: connection }
+      );
+      !ctx[connectionName] && (ctx[connectionName] = models);
+    }
+    ctx.models = Object.assign({}, mongoose.models, ctx.models, models);
     if (!connected) {
       if (lazyConnect) {
         connect(url, options);
